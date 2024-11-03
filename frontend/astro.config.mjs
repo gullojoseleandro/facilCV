64d3ce2cae44511eedfcb766f82b1d10bf39fb32
Jose Leandro Gullo <leandrosec19@gmail.com>
2024-11-03 12:40:43 -0300

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,10 @@
+import MillionLint from '@million/lint';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+import MillionCompiler from '@million/lint';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +14,22 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-  ]
-});
+  ],
+
+  vite: {
+    plugins: [
+      MillionCompiler.vite({
+        mode: "react",
+        server: true,
+        root: "./src",
+      }),
+      MillionLint.vite({
+        enabled: true,
+      })
+    ],
+  },
+
+  output: 'server',
+  adapter: netlify(),
+  },
+)
