a9e88d4530bb5d8339df0ad73729e6fbf221911a
Jose Leandro Gullo <60196978+gullojoseleandro@users.noreply.github.com>
2024-11-07 00:28:28 -0300

    Update Dropdown.jsx

--- frontend/src/components/dropdown/Dropdown.jsx
+++ frontend/src/components/dropdown/Dropdown.jsx
@@ -8,14 +8,14 @@ import {
   } from "@/components/ui/dropdown-menu"
   
 const Dropdown = ({...props}) => {
-    const {children, title, items} = props
+    const {children, title, items, setActiveContent} = props
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
             <DropdownMenuContent>
                 <DropdownMenuLabel className={"text-center"}>{title}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                 {items?.map((item) => <DropdownMenuItem key={item.id} >{item.item}</DropdownMenuItem>)}
+                 {items?.map((item) => <DropdownMenuItem key={item.id} onSelect={()=>setActiveContent(item.component)}>{item.item}</DropdownMenuItem>)}
             </DropdownMenuContent>
         </DropdownMenu>
     )
