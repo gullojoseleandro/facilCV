722892e1021ce23ed7137ddb3e7e5c6de3dd301b
Jose Leandro Gullo <60196978+gullojoseleandro@users.noreply.github.com>
2024-12-01 19:13:35 -0300

    dotenv config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,8 +6,6 @@ import * as dotenv from 'dotenv';
 // Carga manual de variables .env dependiendo del entorno
 dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env' });
 
-console.log("VITE_API_URL cargado manualmente:", process.env.VITE_API_URL);
-
 export default defineConfig({
   integrations: [
     react(),
@@ -15,14 +13,10 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  buildOptions: {
-    site: import.meta.env.VITE_FRONTEND_URL || 'http://localhost:3000', 
-  },
   vite: {
     define: {
-      'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL), 
-      'import.meta.env.VITE_FRONTEND_URL': JSON.stringify(process.env.VITE_FRONTEND_URL), 
+      'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
     },
   },
-  output: 'static', 
+  output: 'static',
 });
--- frontend/src/components/welcome_page/wrapper/WelcomePageWrapper.jsx
+++ frontend/src/components/welcome_page/wrapper/WelcomePageWrapper.jsx
@@ -1,13 +1,6 @@
 import { AuthProvider } from "@/auth/authContext";
 import WelcomePageContent from "@/components/welcome_page/content/WelcomePageContent";
-import.meta.env.VITE_API_URL
 
-if (import.meta.env.VITE_API_URL) {
-    console.log("API URL:", import.meta.env.VITE_API_URL);
-} else {
-    console.log("VITE_API_URL no está definida");
-}
-console.log("Variables de entorno:", import.meta.env);
 const WelcomePageWrapper = () => (
     <AuthProvider>
         <WelcomePageContent />
