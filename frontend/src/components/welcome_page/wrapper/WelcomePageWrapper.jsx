84f736c135be003f8440e4beab7a168a13b20c1e
Jose Leandro Gullo <60196978+gullojoseleandro@users.noreply.github.com>
2024-12-01 18:54:06 -0300

    debug

--- frontend/src/components/welcome_page/wrapper/WelcomePageWrapper.jsx
+++ frontend/src/components/welcome_page/wrapper/WelcomePageWrapper.jsx
@@ -2,7 +2,12 @@ import { AuthProvider } from "@/auth/authContext";
 import WelcomePageContent from "@/components/welcome_page/content/WelcomePageContent";
 import.meta.env.VITE_API_URL
 
-console.log(import.meta.env.VITE_API_URL)
+if (import.meta.env.VITE_API_URL) {
+    console.log("API URL:", import.meta.env.VITE_API_URL);
+} else {
+    console.log("VITE_API_URL no está definida");
+}
+
 const WelcomePageWrapper = () => (
     <AuthProvider>
         <WelcomePageContent />
