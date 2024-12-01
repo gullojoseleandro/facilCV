aaede405c82b5a55e44319b7ea8c55e0043d7a15
Jose Leandro Gullo <60196978+gullojoseleandro@users.noreply.github.com>
2024-12-01 18:32:17 -0300

    debug

--- frontend/src/components/welcome_page/wrapper/WelcomePageWrapper.jsx
+++ frontend/src/components/welcome_page/wrapper/WelcomePageWrapper.jsx
@@ -1,6 +1,8 @@
 import { AuthProvider } from "@/auth/authContext";
 import WelcomePageContent from "@/components/welcome_page/content/WelcomePageContent";
+import.meta.env.VITE_API_URL
 
+console.log(import.meta.env.VITE_API_URL)
 const WelcomePageWrapper = () => (
     <AuthProvider>
         <WelcomePageContent />
